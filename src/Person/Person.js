b21f5b56cc59611ea9443cef9c86398ca7c2f18d
dimitrije888 <dimitrije888@hotmail.com>
2019-03-16 15:25:00 +0100

    Comiting

---
+++ src/Person/Person.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import Radium from 'radium';
+
+import './Person.css';
+
+const person = ( props ) => {
+    const rnd = Math.random();
+
+    if(rnd > 0.7){
+        throw new Error('Something went wrong');
+    }
+    
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default person;   //Radium(person);
